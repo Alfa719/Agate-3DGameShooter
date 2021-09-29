c973d93dd22cd27e4a99d3886602f8a3be1cbb19
Alfaa719 <nurul.hidayat.f3@gmail.com>
2021-09-29 21:00:03 +0700

    First Commit

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;
+    public Text warningText;
+    private Animator anim;                          
+    float restartTimer;
+    public GameObject gameOver, screenFade, warning;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            gameOver.SetActive(true);
+            screenFade.SetActive(true);
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        warning.SetActive(true);
+        anim.SetTrigger("Warning");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100, currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    PlayerShooting playerShooting;
+    bool isDead, damaged;                                         
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooting = GetComponentInChildren<PlayerShooting>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //Jika terkena damage
+        if (damaged)
+        {
+            //Mengubah warna menjadi flash
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            //Fade out damage image
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        //atur nilai damage menjadi false
+        damaged = false;
+    }
+
+    //Memberi Damage
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        //Kurangi hp
+        currentHealth -= amount;
+
+        //Mengubah tampilan dari health slider
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        playerShooting.DisableEffects();
+
+        anim.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }
+    public void RestartLevel()
+    {
+        //meload ulang scene dengan index 0 pada build setting
+        SceneManager.LoadScene(0);
+    }
+}
